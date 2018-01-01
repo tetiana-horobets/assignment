ae310bb888d068b9adb837f77f33029e01f4e419
Michal Schielmann <michalschielmann@gmail.com>
2018-01-01 23:23:32 +0100

    initial commit

---
+++ src/main/java/pl/yameo/internship/assignment/Shape.java
@@ -0,0 +1,10 @@
+package pl.yameo.internship.assignment;
+
+import java.util.List;
+
+public interface Shape {
+	String getName();
+	List<Double> listDimensions();
+	Double calculateArea();
+	Double calculatePerimeter();
+}
